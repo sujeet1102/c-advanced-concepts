044001538106dc3e3d77aabc3700c72dca8465ab
Sujeet1102 <sujeet.22010762@viit.ac.in>
2023-06-28 15:51:22 +0530

    type qualifiers added

--- type-qualifiers/main.c
+++ type-qualifiers/main.c
@@ -13,16 +13,34 @@ can also be used with arrays.
 
 /*
 volatile
+tells compiler that particular variable can change anytime in the runtime of program.
+thus compiler will not compress it during heavy optimizations.
+Use cases:
+- memory mapped peripheral registers.
+- global variables used by ISRs.
+- global variables used by multiple tasks in multi threaded program.
+*/
+
+/*
+NOTE: a variable can be both const and volatile. bcz it can change enexpectedly but program should not try to modify it.
+example: status register in embedded.
 */
 
 /*
 restrict
+used in pointer declaration.
+make sure that a particular pointer is only referance to that particular value.
+so two pointers cannot point to same memory location.
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 
 const float PI = 3.14;
+
+volatile int x;
+
+int * restrict y;
 
 int main(void) {
     
